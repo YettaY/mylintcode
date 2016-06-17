e8ed4be2d048c3490b2d38f024dee602e2fff148
unknown <e_ng_eng@126.com>
2016-06-17 18:45:27 +0800

    g

--- src/g_apac/Round_A_APAC_Test_2016/Googol_String.java
+++ src/g_apac/Round_A_APAC_Test_2016/Googol_String.java
@@ -1,11 +1,13 @@
 package g_apac.Round_A_APAC_Test_2016;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 /**
+ *  Let L(i) denote the length for the string Si.
+ *  L(0) = 0, L(1) =1, L(i) = 2*L(i-1) + 1.
+ *  If we continue the recursive relation L(i) = 2*L(i-1) +1 until to S(0),
+ *  we have L(i)= 2^i - 1.
  * 设位置k的字符是B[k]，k从1开始
  B[k]以下性质
  1. 处在2次幂位置的数必定为0
@@ -14,13 +16,25 @@ import java.util.Scanner;
  */
 public class Googol_String {
     public static void main(String[] args) throws IOException{
-        Scanner in=new Scanner(new File("A-small-practice-1.in"));
-        FileOutputStream fo=new FileOutputStream(new File("out1.txt"));
+        Scanner in=new Scanner(new File("A-large-practice.in"));
+
+        FileWriter fw=new FileWriter("out1.txt");
+        BufferedWriter bw=new BufferedWriter(fw);
+
         int t=in.nextInt();
         for (int i=1;i<=t;i++){
-            int k=Integer.valueOf(in.nextLine());
+            long k=in.nextLong();
             if ((k&(k-1))==0)
-                fo.write();
+                k=0;
+            else{
+                while(k>0 && ((k & 1) == 0))
+                    k >>= 1;
+                k >>= 1;
+                k &= 1;
+            }
+            fw.write("Case #"+i+": "+ k +"\n");
         }
+        bw.close();
+        fw.close();
     }
 }
---
+++ src/g_apac/Round_A_APAC_Test_2016/gCube.java
@@ -0,0 +1,8 @@
+package g_apac.Round_A_APAC_Test_2016;
+
+/**
+ * Created by Administrator on 2016/6/17.
+ */
+public class gCube {
+
+}
