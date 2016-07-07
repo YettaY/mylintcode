f6daaa014f3eeea98b2cd636156377926c99ac15
Yang Lu <ettayang@sohu-inc.com>
2016-07-07 17:11:24 +0800

    g

--- src/leetcode/Hard/Twitter.java
+++ src/leetcode/Hard/Twitter.java
@@ -4,94 +4,83 @@ import java.util.*;
 /**
  * Created by Administrator on 2016/7/5.
  */
-class Tweet {
-    public int user_id;
-    public int tweet_id;
-    public Tweet(int user_id, int tweet_id) {
-        this.user_id=user_id;
-        this.tweet_id=tweet_id;
-    }
-}
+
 public class Twitter {
     /** Initialize your data structure here. */
-    class Node{
-        public int order;
-        public Tweet tweet;
-        public Node(int o,Tweet t){
-            this.order=o;
-            this.tweet=t;
+    class Tweet {
+        int tweet_id;
+        int timestamp;
+        public Tweet(int tid, int time) {
+            this.tweet_id=tid;
+            this.timestamp=time;
         }
     }
-    class SortByOrder implements Comparator{
-        public int compare(Object o1, Object o2) {
-            Node n1=(Node)o1, n2=(Node)o2;
-            return n2.order-n1.order;
-        }
-    }
-    public List<Node> getLastTen(List<Node> tmp){
-        int last=10;
-        if (tmp.size()<10)
-            last=tmp.size();
-        return tmp.subList(tmp.size()-last,tmp.size());
 
-    }
-    public List<Node> getFistTen(List<Node> tmp){
-        int first=10;
-        if (tmp.size()<10)
-            first=tmp.size();
-        return tmp.subList(0,first);
-
-    }
-    private Map<Integer,Set<Integer>> friends;
-    private Map<Integer,List<Node>> users_tweets;
+    private Map<Integer,Set<Integer>> followees;
+    private Map<Integer,List<Tweet>> tweets;
     private int order;
 
+
     public Twitter() {
-        this.friends=new HashMap<Integer,Set<Integer>>();
-        this.users_tweets=new HashMap<Integer,List<Node>>();
+        this.followees=new HashMap<Integer,Set<Integer>>();
+        this.tweets=new HashMap<Integer,List<Tweet>>();
         this.order=0;
     }
 
     /** Compose a new tweet. */
     public void postTweet(int userId, int tweetId) {
-        Tweet tweet =new Tweet(userId,tweetId);
-        if (!users_tweets.containsKey(userId))
-            users_tweets.put(userId, new ArrayList<Node>());
-        order+=1;
-        users_tweets.get(userId).add(new Node(order,tweet));
+        if (!tweets.containsKey(userId)) {
+            tweets.put(userId, new ArrayList<Tweet>());
+            follow(userId,userId);//follow itself
+        }
+        tweets.get(userId).add(0,new Tweet(tweetId, order++));//add new tweet on the first place
     }
 
     /** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
     public List<Integer> getNewsFeed(int userId) {
-        List<Node> tmp=new ArrayList<>();
-        if (users_tweets.containsKey(userId))
-            tmp.addAll(getLastTen(users_tweets.get(userId)));
-        if (friends.containsKey(userId)){
-            for(int user:friends.get(userId)){
-                if (users_tweets.containsKey(user))
-                    tmp.addAll(getLastTen(users_tweets.get(user)));
+        //min heap that the earliest tweet is on the top
+        PriorityQueue<Tweet> q=new PriorityQueue<Tweet>(1, new Comparator<Tweet>(){
+            public int compare(Tweet o1, Tweet o2) {
+                return o1.timestamp-o2.timestamp;
+            }
+        });
+
+        Set<Integer> friends = followees.get(userId);
+        if (friends!=null){
+            for(int fid:friends){
+                List<Tweet> ftweets=tweets.get(fid);
+                if (ftweets!=null) {
+                    for (Tweet t:ftweets){
+                        if (q.size() < 10)
+                            q.add(t);
+                        else{
+                            if(t.timestamp <=q.peek().timestamp)
+                                break;
+                            q.add(t);
+                            q.poll();//remove the oldest tweet
+                        }
+                    }
+                }
             }
         }
-        Collections.sort(tmp,new SortByOrder());
-        Set<Integer> set=new HashSet<>();
-        List<Integer> r=new ArrayList<Integer>();
-        tmp=getFistTen(tmp);
-        for (Node node:tmp)
-            set.add(node.tweet.tweet_id);
-        r.addAll(set);
+        List<Integer> r=new ArrayList<>();
+        while(!q.isEmpty())
+            r.add(0, q.poll().tweet_id);
         return r;
     }
 
     /** Follower follows a followee. If the operation is invalid, it should be a no-op. */
     public void follow(int followerId, int followeeId) {
-        if (!friends.containsKey(followerId))
-            friends.put(followerId,new HashSet<Integer>());
-        friends.get(followerId).add(followeeId);
+        if (!followees.containsKey(followerId))
+            followees.put(followerId,new HashSet<Integer>());
+        followees.get(followerId).add(followeeId);
     }
 
     /** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
     public void unfollow(int followerId, int followeeId) {
-        if (friends.containsKey(followerId))
-            friends.get(followerId).remove(followeeId);
+        if(!followees.containsKey(followeeId) || followeeId==followerId)
+            return;
+        if (followees.containsKey(followerId))
+            followees.get(followerId).remove(followeeId);
     }
 }
